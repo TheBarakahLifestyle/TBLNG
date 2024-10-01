d1b3c1e7100db3e5137d263646006587f2d5eb2e
jimoh-sodiq <jimohsodiq301@gmail.com>
2024-10-01 11:07:26 +0100

    try different blog api admin router

--- src/composables/useJoinUs.ts
+++ src/composables/useJoinUs.ts
@@ -2,7 +2,7 @@ import { TSGhostAdminAPI } from "@ts-ghost/admin-api";
 
 export function useJoinUs() {
   const api = new TSGhostAdminAPI(
-    "https://the-barakah-lifestyle-ng.ghost.io",
+    "https://blog.tblt.com.ng",
     "66f4ee27174b7a0001b91a16:84d44023970fdf76ef98d74a30855edb7954bbbe7e96dcb1a08ab7e06c2f1471",
     "v5.0"
   );
